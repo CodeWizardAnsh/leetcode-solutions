c70b2565c514877c2eb9e788f2a0cb59b7f36d0e
Ansh Gupta <anshgupta9181@gmail.com>
2025-07-09 02:55:47 +0530

    Time: 0 ms (100%), Space: 41.6 MB (81.04%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,15 @@
 class Solution {
-   public int lengthOfLastWord(String s) {
-    String[] arr = s.trim().split("\\s+");  
-    return arr[arr.length - 1].length();
-}
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
 
+        return length;
+    }
 }
