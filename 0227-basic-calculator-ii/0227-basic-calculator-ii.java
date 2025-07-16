d3c58a047f0f92207b63b4b13f48f2f096152e13
Ansh Gupta <anshgupta9181@gmail.com>
2025-07-17 02:41:09 +0530

    Time: 19 ms (63.9%), Space: 46.7 MB (27.99%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -7,12 +7,10 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            // If it's a digit, build the number
             if (Character.isDigit(c)) {
                 num = num * 10 + (c - '0');
             }
-
-            // If it's an operator or end of string, apply the last operator
+            
             if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
                 if (lastOperator == '+') {
                     stack.push(num);
@@ -24,13 +22,11 @@ class Solution {
                     stack.push(stack.pop() / num);
                 }
 
-                // Update operator and reset number
                 lastOperator = c;
                 num = 0;
             }
         }
 
-        // Sum all elements in the stack
         int result = 0;
         for (int val : stack) {
             result += val;
