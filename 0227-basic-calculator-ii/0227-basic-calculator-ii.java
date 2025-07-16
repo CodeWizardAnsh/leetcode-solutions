bdbd4e02b47bb68da8f2918aa883d589cb71d124
Ansh Gupta <anshgupta9181@gmail.com>
2025-07-17 02:39:24 +0530

    Time: 20 ms (58.7%), Space: 46.5 MB (43.63%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,38 +1,41 @@
 class Solution {
     public int calculate(String s) {
-        int result = 0;
-        int lastNum = 0;
+        Stack<Integer> stack = new Stack<>();
         int num = 0;
-        char lastOp = '+'; // initial operator
-
+        char lastOperator = '+';
+        
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
+            // If it's a digit, build the number
             if (Character.isDigit(c)) {
                 num = num * 10 + (c - '0');
             }
 
-            // If c is operator or end of expression
+            // If it's an operator or end of string, apply the last operator
             if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
-                if (lastOp == '+') {
-                    result += lastNum;
-                    lastNum = num;
-                } else if (lastOp == '-') {
-                    result += lastNum;
-                    lastNum = -num;
-                } else if (lastOp == '*') {
-                    lastNum = lastNum * num;
-                } else if (lastOp == '/') {
-                    lastNum = lastNum / num;
+                if (lastOperator == '+') {
+                    stack.push(num);
+                } else if (lastOperator == '-') {
+                    stack.push(-num);
+                } else if (lastOperator == '*') {
+                    stack.push(stack.pop() * num);
+                } else if (lastOperator == '/') {
+                    stack.push(stack.pop() / num);
                 }
 
-                // Reset for next number
-                lastOp = c;
+                // Update operator and reset number
+                lastOperator = c;
                 num = 0;
             }
         }
 
-        result += lastNum; // add the last remaining number
+        // Sum all elements in the stack
+        int result = 0;
+        for (int val : stack) {
+            result += val;
+        }
+
         return result;
     }
 }
