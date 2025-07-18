be7ca2aca5bef2c68216921d33ad83c6c5effccf
Ansh Gupta <anshgupta9181@gmail.com>
2025-07-19 02:13:06 +0530

    Time: 11 ms (48.67%), Space: 44.6 MB (99.83%) - LeetHub

--- 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.java
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.java
@@ -3,24 +3,17 @@ class Solution {
         int mod = 1_000_000_007;
         long ans = 0;
         int count = 1;
-
-        // Go through each character of the string
         for (int i = 1; i < s.length(); i++) {
-            // If current character is same as previous one, increase count
             if (s.charAt(i) == s.charAt(i - 1)) {
                 count++;
             } else {
-                // Add total substrings from previous group
                 ans += (long) count * (count + 1) / 2;
                 ans %= mod;
-                count = 1;  // reset count for new character
+                count = 1;  
             }
         }
-
-        // Add the last group of same characters
         ans += (long) count * (count + 1) / 2;
         ans %= mod;
-
         return (int) ans;
     }
 }
