0446b54fe2e0c70a8c39c05406bcc2d1b6f9e63f
Ansh Gupta <anshgupta9181@gmail.com>
2025-07-20 16:37:48 +0530

    Time: 1 ms (99.49%), Space: 44.7 MB (10.06%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,26 +1,20 @@
 class Solution {
     public int compress(char[] chars) {
         if (chars.length == 1) return 1;
-
-        int write = 0; // Position to write in-place
+        int write = 0; 
         int count = 1;
-
         for (int i = 0; i < chars.length; i++) {
-            // Check if next char is same
             if (i + 1 < chars.length && chars[i] == chars[i + 1]) {
                 count++;
             } else {
-                // Write the character
                 chars[write++] = chars[i];
-
-                // If count > 1, write the digits of count
                 if (count > 1) {
                     String str = Integer.toString(count);
                     for (int k = 0; k < str.length(); k++) {
                         chars[write++] = str.charAt(k);
                     }
                 }
-                count = 1; // Reset count for next group
+                count = 1; 
             }
         }
 
